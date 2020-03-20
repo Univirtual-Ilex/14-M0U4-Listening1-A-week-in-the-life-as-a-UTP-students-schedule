225a5b25df3a46e8b232f36f27e2eae731d17b35
Alex Agudelo <agara85@gmail.com>
2020-03-20 17:50:04 -0500

    adding binaries content

--- src/components/Actividad1/Actividad_data.js
+++ src/components/Actividad1/Actividad_data.js
@@ -2,25 +2,25 @@ const data = [
     {
       "id": 30,
       "video": "video 1",
-      "media": "./media/video.mp4",
+      "media": "./media/video1.mp4",
 
     },
 
     {
       "id": 31,
       "video": "video 2",
-      "media": "./media/video.mp4",
+      "media": "./media/video2.mp4",
     },
     {
       "id": 32,
       "video": "video 3",
-      "media": "./media/video.mp4",
+      "media": "./media/video3.mp4",
      
     },
     {
       "id": 33,
       "video": "video 4",
-      "media": "./media/video.mp4",
+      "media": "./media/video4.mp4",
      
     },
 
