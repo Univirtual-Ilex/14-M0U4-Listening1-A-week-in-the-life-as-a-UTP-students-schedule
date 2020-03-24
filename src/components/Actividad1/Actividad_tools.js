6274a103b19de3ea8e067a5ea878c400e8d54a2c
Alex Agudelo <agara85@gmail.com>
2020-03-24 11:43:29 -0500

    ready for production

--- src/components/Actividad1/Actividad_tools.js
+++ src/components/Actividad1/Actividad_tools.js
@@ -3,121 +3,121 @@ const tools = [
     "id":1,
     "title": "First",
     "text": "1st",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/1st.mp3"
 },
 {
     "id":2,
     "title": "Second",
     "text": "2nd",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/2nd.mp3"
 },
 {
     "id":3,
     "title": "Third",
     "text": "3rd",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/3rd.mp3"
 },
 {
     "id":4,
     "title": "Fourth",
     "text": "4th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/4th.mp3"
 },
 {
     "id":5,
     "title": "Fiveth",
     "text": "5th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/5th.mp3"
 },
 {
     "id":6,
     "title": "Sixth",
     "text": "6th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/6th.mp3"
 },
 {
     "id":7,
     "title": "Seventh",
     "text": "7th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/7th.mp3"
 },
 {
     "id":8,
     "title": "Eighth",
     "text": "4th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/8th.mp3"
 },
 {
     "id":9,
     "title": "Ninth",
     "text": "9th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/9th.mp3"
 },
 {
     "id":10,
     "title": "Tenth",
     "text": "10th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/10th.mp3"
 },
 {
     "id":11,
     "title": "Eleventh",
     "text": "11th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/11th.mp3"
 },
 {
     "id":12,
     "title": "Twelfth",
     "text": "12th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/12th.mp3"
 },
 {
     "id":13,
     "title": "Thirteenth",
     "text": "13th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/13th.mp3"
 },
 {
     "id":14,
     "title": "Fourteenth",
     "text": "14th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/14th.mp3"
 },
 {
     "id":15,
     "title": "Fifteenth",
     "text": "15th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/15th.mp3"
 },
 {
     "id":16,
     "title": "Sixteenth",
     "text": "16th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/16th.mp3"
 },
 {
     "id":17,
     "title": "Seventeenth",
     "text": "17th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/17th.mp3"
 },
 {
     "id":18,
     "title": "Eighteenth",
     "text": "18th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/18th.mp3"
 },
 {
     "id":19,
     "title": "Nineteenth",
     "text": "19th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/19th.mp3"
 },
 {
     "id":20,
     "title": "Twentieth",
     "text": "20th",
-    "audio": "./media/audio.mp3"
+    "audio": "./media/20th.mp3"
 },
 ]
 
--- src/components/Actividad1/Actividad_tools1.js
+++ src/components/Actividad1/Actividad_tools1.js
@@ -2,26 +2,26 @@ const tools1 = [
 {
     "id":1,
     "title": "FINE ARTS SCHOOL",
-    "audio": "./media/audio.mp3",
+    "audio": "./media/fine-arts-school.mp3",
     "programs": ["Bachelor's degree in Philosophy", "Bachelor's degree in Music", "Bachelor's degree in Visual Arts", "Bachelor's degree in Bilingualism"]
 },
 {
     "id":2,
     "title": "ENVIRONMENT MANAGEMENT SCHOOL",
-    "audio": "./media/audio.mp3",
+    "audio": "./media/environmental-management-school.mp3",
     "programs":[ "Technology in Sustainable Administration", "Sustainable Tourism Administration", "Environmental Management", ]   
 },
 {
     "id": 3,
     "title": "BASIC SCIENCES SCHOOL",
-    "audio": "./media/audio.mp3",
+    "audio": "./media/basic-sciences-school.mp3",
     "programs": [ "Bachelor's degree un Math and Physics" ] 
     
 },
 {
     "id":4,
     "title": "EDUCATIONAL SCIENCES SCHOOL",
-    "audio": "./media/audio.mp3",
+    "audio": "./media/educational-sciences-school.mp3",
     "programs": [
         "Bachelor's degree in Ethinic Education" , "Early Childhood Education", "Bachelo's degree in communication and educational informatics" , "Bachelor's degree in Spanish and Literature"
     ] 
@@ -30,38 +30,38 @@ const tools1 = [
 {
     "id":5,
     "title": "HEALTH SCIENCES SCHOOL",
-    "audio": "./media/audio.mp3",
+    "audio": "./media/health-sciences-school.mp3",
     "programs": ["Veterinary Medicine", "Medicine", "Emergency Medical Technician (EMT)", " Sports and Recreation Sciences"] 
       
 },
 {
     "id":6,
     "title": "INDUSTRIAL ENGINEERING SCHOOL",
-    "audio": "./media/audio.mp3",
+    "audio": "./media/industrial-engineering-school.mp3",
     "programs": ["Industrial Engineering"]
 },
 {
     "id": 7,
     "title": "MECHANICAL ENGINEERING SCHOOL",
-    "audio": "./media/audio.mp3",
+    "audio": "./media/mechanical-engineering-school.mp3",
     "programs": ["Mechanical Engineering"]
 },
 {
     "id":8,
     "title": "ENGINEERING SCHOOL",
-    "audio": "./media/audio.mp3",
+    "audio": "./media/engineering-school.mp3",
     "programs": ["Electronic Engineering", "Physics engineering", "Computer Science Engineering", "Electrical Engineering"]
 },
 {
     "id":9,
     "title": "TECHNOLOGY SCHOOL",
-    "audio": "./media/audio.mp3",
+    "audio": "./media/technology-school.mp3",
     "programs": ["Chemical Technology", "Mechanical Technology", "Electrical Technology", "Industrial Technology", "Industrial Chemistry", "Professional Technician in Mechatronics", "Mechatronics Technology", "Mechatronics Engineering"]
 },
 {
     "id": 10,
     "title": "SCHOOL OF AGRICULTURAL SCIENCES AND AGRIBUSINESS",
-    "audio": "./media/audio.mp3",
+    "audio": "./media/school-agriculture-sciences-and-agrobusiness.mp3",
     "programs": ["Engineering in Sustainable Processes of the Woods", "Engineering in Agroindustrial Processes"]
 },
 ]
